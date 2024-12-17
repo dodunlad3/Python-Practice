6845b1fe094c12664ec8c916dc6da7bee4981741
dodunlad3 <75912431+dodunlad3@users.noreply.github.com>
2024-12-16 19:17:10 -0500

    Made Number Guessing Game

--- Number Guesser/main.py
+++ Number Guesser/main.py
@@ -3,16 +3,18 @@ import random
 
 from random import randint
 
+# Generate a random number between 1 and 100
 number = random.randint(1, 100)
-
-
-guess = int()
+# Set the number of lives
 lives = 7
 
 print("Welcome to the Python Practice\n")
 print("You have 7 lives")
 
-
+# Ask the user to guess the number
+# If the guess is higher than the number, tell the user "Too high"
+# If the guess is lower than the number, tell the user "Too low"
+# If the guess is correct, tell the user "You got it"
 while lives > 0:
     guess = int(input("Guess a number: "))
     if guess == number:
@@ -26,6 +28,7 @@ while lives > 0:
         print("Too low, try again")
     print("You have " + str(lives) + " lives")
 
+# If the user runs out of lives, tell them "You lost"
 if lives == 0:
     print("You lost")
 
